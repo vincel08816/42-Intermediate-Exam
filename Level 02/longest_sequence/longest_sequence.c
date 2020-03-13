da4387f885321a05be357602a5b120e1c7b59f72
Vincent <vince.l08816@gmail.com>
2020-03-12 17:39:42 -0700

    longest_sequence added

--- Level 02/longest_sequence/longest_sequence.c
+++ Level 02/longest_sequence/longest_sequence.c
@@ -6,7 +6,7 @@
 /*   By: vilee <vilee@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 16:53:01 by vilee             #+#    #+#             */
-/*   Updated: 2020/03/12 17:35:51 by vilee            ###   ########.fr       */
+/*   Updated: 2020/03/12 17:39:33 by vilee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,67 +50,67 @@ int		longest_sequence(struct s_node *node)
 	return (ret);
 }
 
-#include <stdio.h>
-#define NODE(v,l,r) &(t_node){v,l,r}
+// #include <stdio.h>
+// #define NODE(v,l,r) &(t_node){v,l,r}
 
-int		main(void)
-{
-	// 	Example 1:
+// int		main(void)
+// {
+// 	// 	Example 1:
 
-	//        10
-	//        /
-	//       5
-	//      / \
-	//     /   \
-	//    6    9
-	//   / \
-	//  /   \
-	// 7   13
-	//	Answer should be 3 (5->6->7)
+// 	//        10
+// 	//        /
+// 	//       5
+// 	//      / \
+// 	//     /   \
+// 	//    6    9
+// 	//   / \
+// 	//  /   \
+// 	// 7   13
+// 	//	Answer should be 3 (5->6->7)
 
-	t_node *example1 = 
-		NODE(10, 
-			NODE(5, 
-				NODE(6, NODE(7,0,0), NODE(13,0,0)),
-				NODE(9,0,0)),
-			0);
-	printf("Example 1 = %d\n", longest_sequence(example1));
-	// 	Example 2:
+// 	t_node *example1 = 
+// 		NODE(10, 
+// 			NODE(5, 
+// 				NODE(6, NODE(7,0,0), NODE(13,0,0)),
+// 				NODE(9,0,0)),
+// 			0);
+// 	printf("Example 1 = %d\n", longest_sequence(example1));
+// 	// 	Example 2:
 
-	//    5
-	//   / \
-	//  /   \
-	// 6     4
-	//     /  \
-	//    9    3
-	//  /  \    \
-	// 3    2    2
-	// Answer should be 2 (5->6)
+// 	//    5
+// 	//   / \
+// 	//  /   \
+// 	// 6     4
+// 	//     /  \
+// 	//    9    3
+// 	//  /  \    \
+// 	// 3    2    2
+// 	// Answer should be 2 (5->6)
 
-	t_node *example2 =
-		NODE(5,
-			NODE(6,0,0),
-			NODE(4,
-				NODE(9,
-					NODE(3,0,0),
-					NODE(2,0,0)), 
-				NODE(3,0,
-					NODE(2,0,0))));
-	printf("Example 2 = %d\n", longest_sequence(example2));
-	// Example 3: 
+// 	t_node *example2 =
+// 		NODE(5,
+// 			NODE(6,0,0),
+// 			NODE(4,
+// 				NODE(9,
+// 					NODE(3,0,0),
+// 					NODE(2,0,0)), 
+// 				NODE(3,0,
+// 					NODE(2,0,0))));
+// 	printf("Example 2 = %d\n", longest_sequence(example2));
+// 	// Example 3: 
 
-	//     30
-	//     / \
-	//    /   \
-	//   15   41
-	//  /     /
-	// 61    80
-	t_node *example3 = 
-		NODE(30, 
-			NODE(15,NODE(61,0,0),0), 
-			NODE(41,NODE(80,0,0),0));
-	printf("Example 3 = %d\n", longest_sequence(example3));
+// 	//     30
+// 	//     / \
+// 	//    /   \
+// 	//   15   41
+// 	//  /     /
+// 	// 61    80
+// 	t_node *example3 = 
+// 		NODE(30, 
+// 			NODE(15,NODE(61,0,0),0), 
+// 			NODE(41,NODE(80,0,0),0));
+// 	printf("Example 3 = %d\n", longest_sequence(example3));
 	
-	t_node *example4 = NULL;
-	printf("Example 14= %d\n", longest_sequence(example4));
-}
+// 	t_node *example4 = NULL;
+// 	printf("Example 14= %d\n", longest_sequence(example4));
+// }
